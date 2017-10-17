b71561f5b7cfdab9196bd61a21137720318ef72b
Wator Vapor <watorvapor@gmail.com>
2017-10-17 10:33:10 +0900

    Update usb_intf.c

--- 8821au-1.0/os_dep/linux/usb_intf.c
+++ 8821au-1.0/os_dep/linux/usb_intf.c
@@ -312,7 +312,7 @@ static struct usb_device_id rtw_usb_id_tbl[] ={
 	{USB_DEVICE(0x2001, 0x3314),.driver_info = RTL8821}, /* D-Link - Cameo */
 	{USB_DEVICE(0x2001, 0x3318),.driver_info = RTL8821}, /* D-Link - Cameo */
 	{USB_DEVICE(0x0E66, 0x0023),.driver_info = RTL8821}, /* HAWKING - Edimax */
-   	{USB_DEVICE(0x0411, 0x029b),.driver_info = RTL8821}, /* Buffalo - WI-U2-433DHP */
+	{USB_DEVICE(0x0411, 0x029b),.driver_info = RTL8821}, /* Buffalo - WI-U2-433DHP */
 #endif
 
 #ifdef CONFIG_RTL8192E
